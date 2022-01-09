5936483fce92864b0810a5095fd2a89b70e62f16
ZaikoXander <alexdaniel__@outlook.com>
2022-01-09 12:25:57 -0300

    modulo2

---
+++ CeV/Site-Fofo/script.js
@@ -0,0 +1,46 @@
+let menuglobal = [false, false, false, false];
+function fofo_nav(menu) {
+    let nav_menus = document.querySelector('nav.nav-menus');
+    let simbolo = document.getElementsByClassName('simbolo');
+    let nav_menus_tutoriais = document.querySelector('div.nav-menus-tutoriais');
+    let nav_menus_referencias = document.querySelector('div.nav-menus-referencias');
+    let nav_menus_exercicios = document.querySelector('div.nav-menus-exercicios');
+    let nav_menus_videos = document.querySelector('div.nav-menus-videos');
+    if(!menuglobal.includes(true)) {
+        nav_menus.style.display = 'block';
+        switch (menu) {
+            case 'tutoriais':
+                nav_menus_tutoriais.style.display = 'block';
+                simbolo[0].innerHTML = '&utrif;';
+                menuglobal[0] = true;
+                break;
+            case 'referencias':
+                nav_menus_referencias.style.display = 'block';
+                simbolo[1].innerHTML = '&utrif;';
+                menuglobal[1] = true;
+                break;
+                case 'exercicios':
+                nav_menus_exercicios.style.display = 'block';
+                simbolo[2].innerHTML = '&utrif;';
+                menuglobal[2] = true;
+                break;
+            case 'videos':
+                nav_menus_videos.style.display = 'block';
+                simbolo[3].innerHTML = '&utrif;';
+                menuglobal[3] = true;
+                break;
+            default:
+                break;
+        }
+    } else {
+        nav_menus.style.display = '';
+        simbolo[0].innerHTML = '&dtrif;';
+        simbolo[1].innerHTML = '&dtrif;';
+        simbolo[2].innerHTML = '&dtrif;';
+        simbolo[3].innerHTML = '&dtrif;';
+        nav_menus_tutoriais.style.display = '';
+        nav_menus_referencias.style.display = '';
+        nav_menus_exercicios.style.display = '';
+        nav_menus_videos.style.display = '';
+    }
+}
